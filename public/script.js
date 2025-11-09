e818d8d5c70fb0f530d0c6e5030f211573a23d58
Shayan <shayan2396@gmail.com>
2025-11-09 20:04:40 +0330

    configuring dotenv

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
-const baseUrl = "http://localhost:3000";
+const port = process.env.PORT;
+const baseUrl = "http://localhost:" + port;
 const socket = io(baseUrl, {
     withCredentials: true,
     transports: ['websocket', 'polling']
